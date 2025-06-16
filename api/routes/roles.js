19e2de006588f983218e293472b269a8e16eaceb
jusiann <justee307@gmail.com>
2025-06-16 15:17:04 +0300

    laptop01-commit

---
+++ api/routes/roles.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+const isAuthenticated = true;
+router.all("*", (req, res, next) => {
+  if(isAuthenticated) {
+    next();
+} else {
+  res.json({
+    "success": false,
+    "error": "Not Auth"
+  })
+}
+})
+router.get("/:id", (req, res, next) => {
+  res.json({
+    body: req.body,
+    params: req.params,
+    query: req.query,
+    headers: req.headers
+  })
+});
+
+module.exports = router;
